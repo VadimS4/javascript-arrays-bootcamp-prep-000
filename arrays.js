ae4727ac0ce35b698ad3bd4c192b4c4268c25142
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-03-14 10:33:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,52 @@
-  var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-  
-  function addElementToBeginningOfArray(array, element)
-  {
-    return(element, ...array);
-  }
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element)
+{
+  return(element, ...array);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element)
+{
+  return(...array, element);
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, element)
+{
+  return array[index];
+}
+
+function destructivelyRemoveElementToBeginningOfArray(array)
+{
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array)
+{
+  array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop();
+  return array;
+}
+
+function RemoveElementFromEndOfArray(array)
+{
+  array.slice(0, array.length - 1);
+  return array;
+}
